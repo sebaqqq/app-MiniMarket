c7a60b25be75a73591efcf134dc893f46be44b69
fernet_en_mano <nob96472@gmail.com>
2024-04-11 18:47:10 -0400

    historial de ventas

--- screens/Historial.js
+++ screens/Historial.js
@@ -7,18 +7,26 @@ const Historial = () => {
   const [historial, setHistorial] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [totalPorFecha, setTotalPorFecha] = useState({});
 
   useEffect(() => {
     const fetchHistorial = async () => {
       try {
-        const historialCollection = collection(db, 'historialVenta');
+        const historialCollection = collection(db, 'historialVentas');
         const historialSnapshot = await getDocs(historialCollection);
-        const historialData = historialSnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
+        const historialData = historialSnapshot.docs.map(doc => {
+          const data = doc.data();
+          // Convierte el campo totalCompra de string a number
+          const totalCompra = parseFloat(data.totalCompra);
+          return {
+            id: doc.id,
+            ...data,
+            totalCompra: isNaN(totalCompra) ? 0 : totalCompra // Si es NaN, asigna 0
+          };
+        });
         setHistorial(historialData);
         setLoading(false);
+        calcularTotalPorFecha(historialData);
       } catch (error) {
         console.error("Error fetching historial:", error);
         setError(error);
@@ -28,13 +36,32 @@ const Historial = () => {
     fetchHistorial();
   }, []);
 
+  // Función para calcular el total de compras por fecha
+  const calcularTotalPorFecha = (historialData) => {
+    const totalPorFecha = {};
+    historialData.forEach(item => {
+      const fecha = formatFecha(item.fecha);
+      totalPorFecha[fecha] = (totalPorFecha[fecha] || 0) + item.totalCompra;
+    });
+    setTotalPorFecha(totalPorFecha);
+  };
+
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
       <Text>ID: {item.id}</Text>
-      <Text>Fecha: {item.fecha}</Text>
+      <Text>Fecha: {formatFecha(item.fecha)}</Text>
       <Text>Total Compra: {item.totalCompra}</Text>
     </View>
   );
+
+  const formatFecha = (fecha) => {
+    const fechaObj = new Date(fecha);
+    return fechaObj.toLocaleDateString('es-ES', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    });
+  };
 
   if (loading) {
     return (
@@ -53,11 +80,19 @@ const Historial = () => {
   }
 
   return (
-    <FlatList
-      data={historial}
-      renderItem={renderItem}
-      keyExtractor={item => item.id}
-    />
+    <View style={styles.container}>
+      <FlatList
+        data={historial}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <View style={styles.totalContainer}>
+        <Text style={styles.totalText}>Total por Día:</Text>
+        {Object.keys(totalPorFecha).map(fecha => (
+          <Text key={fecha}>{fecha}: {totalPorFecha[fecha]}</Text>
+        ))}
+      </View>
+    </View>
   );
 };
 
@@ -68,12 +103,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   itemContainer: {
+    width:350,
     borderWidth: 1,
     borderColor: '#ccc',
     padding: 10,
     margin: 5,
     borderRadius: 5,
   },
+  totalContainer: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  totalText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  }
 });
 
 export default Historial;
