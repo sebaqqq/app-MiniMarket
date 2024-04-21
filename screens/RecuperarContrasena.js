ddd8e01848430ec4daf1af68a349fc901ae9abce
Sebastián Quintana <101677885+sebaqqq@users.noreply.github.com>
2024-04-21 11:09:10 -0400

    cambio paquetes

--- screens/RecuperarContrasena.js
+++ screens/RecuperarContrasena.js
@@ -5,7 +5,7 @@ import {
   TextInput,
   TouchableOpacity,
   Alert,
-  Image
+  Image,
 } from "react-native";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../DB/firebase";
@@ -21,23 +21,26 @@ const ForgotPassword = () => {
         setEmailSent(true);
       })
       .catch((error) => {
-        Alert.alert("Error", "No se pudo enviar el correo electrónico de restablecimiento de contraseña.");
+        Alert.alert(
+          "Error",
+          "No se pudo enviar el correo electrónico de restablecimiento de contraseña."
+        );
       });
   };
 
   return (
-    
     <View style={LoginStyles.container}>
       <View style={LoginStyles.centeredContainer}>
-  
-        <Image 
-        source={require("../images/panda_recuperar.png")} 
-        style={{ marginTop: -200 }} // Cambia el valor de marginTop según sea necesario
-        />        
-          <Text style={LoginStyles.title}>Olvidaste</Text>
-          <Text style={LoginStyles.headerTextrecuperar}>tu Contraseña?</Text>
+        <Image
+          source={require("../images/panda_recuperar.png")}
+          style={{ marginTop: -200 }}
+        />
+        <Text style={LoginStyles.title}>Olvidaste</Text>
+        <Text style={LoginStyles.headerTextrecuperar}>tu Contraseña?</Text>
 
-        <Text style={{ marginTop:10, marginBottom:10 }}>ingresa tu correo para recupeararla</Text>
+        <Text style={{ marginTop: 10, marginBottom: 10 }}>
+          ingresa tu correo para recupeararla
+        </Text>
         <TextInput
           placeholder="Correo electrónico"
           style={LoginStyles.inputRecupear}
@@ -45,7 +48,7 @@ const ForgotPassword = () => {
           value={email}
           keyboardType="email-address"
         />
-        
+
         <TouchableOpacity
           style={LoginStyles.loginButton}
           onPress={handleForgotPassword}
@@ -53,17 +56,13 @@ const ForgotPassword = () => {
           <Text style={LoginStyles.buttonText}>Restablecer contraseña</Text>
         </TouchableOpacity>
         {emailSent && (
-          <Text style={LoginStyles.successText}>Se ha enviado un correo electrónico de restablecimiento de contraseña a {email}. Por favor, revisa tu bandeja de entrada.</Text>
+          <Text style={LoginStyles.successText}>
+            Se ha enviado un correo electrónico de restablecimiento de
+            contraseña a {email}. Por favor, revisa tu bandeja de entrada.
+          </Text>
         )}
-        
       </View>
     </View>
-
-
-
-
-
-
   );
 };
 
