44c7895968c1074a1863dc0dc10802ba157d40bf
Sebastián Quintana <101677885+sebaqqq@users.noreply.github.com>
2024-04-21 10:38:31 -0400

    sacando error firebase

--- DB/firebase.js
+++ DB/firebase.js
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkVflYZa7XfUBFmAJomRvYjVQdb2tW8Jo",
@@ -11,11 +9,10 @@ const firebaseConfig = {
   projectId: "saga-ed938",
   storageBucket: "saga-ed938.appspot.com",
   messagingSenderId: "49283180293",
-  appId: "1:49283180293:web:4a2ad3156f8714f590396c"
+  appId: "1:49283180293:web:4a2ad3156f8714f590396c",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
